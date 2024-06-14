2747b031f2d98bdcee7e76e69a44d9600a967730
Greg Divis <gdivis@inedo.com>
2024-06-13 21:28:25 -0400

    refactor vulns audit into two separate commands

---
+++ pgutil/Vulns/AssessCommand.cs
@@ -0,0 +1,78 @@
+﻿using ConsoleMan;
+
+namespace PgUtil;
+
+internal sealed partial class VulnsCommand
+{
+    internal sealed class AssessCommand : IConsoleCommand
+    {
+        public static string Name => "assess";
+        public static string Description => "Assess a vulneability by specifying its ID";
+
+        public static void Configure(ICommandBuilder builder)
+        {
+            builder.WithOption<VulnIdOption>()
+                .WithOption<AssessmentTypeOption>()
+                .WithOption<CommentOption>()
+                .WithOption<PolicyOption>();
+        }
+
+        public static async Task<int> ExecuteAsync(CommandContext context, CancellationToken cancellationToken)
+        {
+            var client = context.GetProGetClient();
+
+            var id = context.GetOption<VulnIdOption>();
+            var type = context.GetOption<AssessmentTypeOption>();
+            _ = context.TryGetOption<CommentOption>(out var comment);
+            _ = context.TryGetOption<PolicyOption>(out var policy);
+
+            CM.Write("Assessing ", new TextSpan(id, ConsoleColor.White), " as ", new TextSpan(type, ConsoleColor.White));
+
+            if (!string.IsNullOrWhiteSpace(policy))
+                CM.Write(" with policy ", new TextSpan(policy, ConsoleColor.White));
+
+            if (!string.IsNullOrWhiteSpace(comment))
+            {
+                Console.Write(" (comment: ");
+                WordWrapper.WriteOutput(comment);
+                Console.Write(')');
+            }
+
+            Console.WriteLine("...");
+
+            await client.AssessVulnerabilityAsync(id, type, comment, policy, cancellationToken);
+
+            Console.WriteLine("Assessment completed.");
+
+            return 0;
+        }
+
+        private sealed class VulnIdOption : IConsoleOption
+        {
+            public static bool Required => true;
+            public static string Name => "--id";
+            public static string Description => "ID of the vulnerability to assess";
+        }
+
+        private sealed class AssessmentTypeOption : IConsoleOption
+        {
+            public static bool Required => true;
+            public static string Name => "--type";
+            public static string Description => "Assessment type in ProGet";
+        }
+
+        private sealed class CommentOption : IConsoleOption
+        {
+            public static bool Required => false;
+            public static string Name => "--comment";
+            public static string Description => "Comment to add to the vulnerability in ProGet";
+        }
+
+        private sealed class PolicyOption : IConsoleOption
+        {
+            public static bool Required => false;
+            public static string Name => "--policy";
+            public static string Description => "ProGet policy to apply to the assessment";
+        }
+    }
+}
---
+++ pgutil/Vulns/AuditCommand.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleMan;
+using Inedo.DependencyScan;
+using Inedo.ProGet;
+
+namespace PgUtil;
+
+internal sealed partial class VulnsCommand
+{
+    internal sealed class AuditCommand : IConsoleCommand
+    {
+        public static string Name => "audit";
+        public static string Description => "List vulnerabilities associated with a package or project file";
+
+        public static void Configure(ICommandBuilder builder)
+        {
+            builder.WithOption<PackageTypeOption>()
+                .WithOption<ProjectOption>();
+        }
+
+        public static async Task<int> ExecuteAsync(CommandContext context, CancellationToken cancellationToken)
+        {
+            var client = context.GetProGetClient();
+
+            var input = context.GetOption<ProjectOption>();
+
+            var scannerType = DependencyScannerType.Auto;
+            if (context.TryGetOption<PackageTypeOption>(out var type))
+            {
+                if (!Enum.TryParse(type, true, out scannerType))
+                {
+                    CM.WriteError<PackageTypeOption>("Invalid type specified.");
+                    return -1;
+                }
+            }
+
+            CM.WriteLine("Scanning for dependencies in ", new TextSpan(input, ConsoleColor.White), "...");
+
+            var scanner = DependencyScanner.GetScanner(new CreateDependencyScannerArgs(input, SourceFileSystem.Default), scannerType);
+            var projects = await scanner.ResolveDependenciesAsync(cancellationToken);
+            if (projects.Count == 0)
+            {
+                CM.WriteError($"No projects found in input path: {input}");
+                return -1;
+            }
+
+            var dependencyLookup = new Dictionary<PackageVersionIdentifier, List<string>>();
+            foreach (var p in projects)
+            {
+                foreach (var d in p.Dependencies)
+                {
+                    var pvi = new PackageVersionIdentifier(d.Type!, d.Name, d.Version, d.Group);
+
+                    if (!dependencyLookup.TryGetValue(pvi, out var projs))
+                    {
+                        projs = [];
+                        dependencyLookup[pvi] = projs;
+                    }
+
+                    projs.Add(p.Name);
+                }
+            }
+
+            var infos = dependencyLookup.Select(d => new DependencyInfo(d.Key, d.Value)).ToList();
+
+            CM.WriteLine($"Found {infos.Count} package dependencies. Performing audit...");
+
+            return await AuditAsync(client, infos, cancellationToken);
+        }
+
+        private sealed class PackageTypeOption : IConsoleOption
+        {
+            public static bool Required => false;
+            public static string Name => "--type";
+            public static string Description => "Type of package to audit for vulnerabilities";
+            public static string[] ValidValues => ["apk", "deb", "maven", "nuget", "conda", "cran", "helm", "npm", "pypi", "rpm", "gem"];
+        }
+
+        private sealed class ProjectOption : IConsoleOption
+        {
+            public static bool Required => true;
+            public static string Name => "--project";
+            public static string Description => "Project to audit for vulnerable packages";
+        }
+    }
+}
