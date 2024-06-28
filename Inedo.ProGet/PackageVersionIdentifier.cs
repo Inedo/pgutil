9c91f739a5aac839426a566fc5338ff23bf31f37
Alex Vasse <alexandervasse@gmail.com>
2024-06-28 10:10:20 +0900

    CS file updates - vulns command

--- Inedo.ProGet/PackageVersionIdentifier.cs
+++ Inedo.ProGet/PackageVersionIdentifier.cs
@@ -1,7 +1,37 @@
-﻿namespace Inedo.ProGet;
+﻿/*******************************************************************************
+* ABOUT THIS FILE                                                              *
+********************************************************************************
+*                                                                              *
+* This file contains C# code to serialize (write) and deserialize (read) JSON  *
+* objects for ProGet HTTP Endpoints. It also serves as the specifications for  *
+* the expected format of these JSON objects; this is why it doesn't follow     *
+* normal C# commenting contentions/standards.                                  *
+*                                                                              *
+* If you're not familiar with JSON Serialization in C#, a few notes:           *
+*                                                                              *
+* - The C# property names are PascalCase, but they are converted to camelCase  *
+*   for JSON; e.g. "MyProperty" will become "myProperty"                       *
+*                                                                              *
+* - Some C# properties will reference enums; these are converted to camelCase  *
+*   string values; e.g. "MyValue" will become "myValue"                        *
+*                                                                              *
+* - Date types are forgiving, but you should specify ISO8601 and C# will       *
+*   outputs something like "2019-08-01T00:00:00-07:00"                         *
+*                                                                              *
+* - A type with a ? (e.g. int?) means that the JSON property may be missing    *
+*   or null. This usually means that it's an optional property, but it also    *
+*   could mean that it's required only in some contexts                        *
+*                                                                              *
+* - The "required" keyword means that the JSON property should always be       *
+*   present (when listing) or must be specified when creating/editing          *
+*                                                                              *
+*******************************************************************************/
 
+namespace Inedo.ProGet;
+
+// JSON Object used by the Audit Vulnerability HTTP endpoint
 public sealed record class PackageVersionIdentifier(
-
+    // The package type (e.g. "nuget")
     string Type,
 
     string Name,
