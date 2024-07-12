01a912887768e00e01c209becf9d3002acbc1648
Alex Vasse <alexandervasse@gmail.com>
2024-07-12 16:34:00 +0900

    buildIssue and projectInfo done

--- Inedo.ProGet/ProjectInfo.cs
+++ Inedo.ProGet/ProjectInfo.cs
@@ -1,9 +1,47 @@
-﻿namespace Inedo.ProGet;
+﻿/*******************************************************************************
+* ABOUT THIS FILE                                                              *
+********************************************************************************
+*                                                                              *
+* This file contains C# code to serialize (write) and deserialize (read) JSON  *
+* objects for ProGet HTTP Endpoints. It also serves as the specifications for  *
+* the expected format of these JSON objects; this is why it doesn't follow     *
+* normal C# commenting contentions/standards.                                  *
+*                                                                              *
+* If you're not familiar with JSON Serialization in C#, a few notes:           *
+*                                                                              *
+* - The C# property names are PascalCase, but they are converted to camelCase  *
+*   for JSON; e.g. "MyProperty" will become "myProperty"                       *
+*                                                                              *
+* - Some C# properties will reference enums; these are converted to camelCase  *
+*   string values; e.g. "MyValue" will become "myValue"                        *
+*                                                                              *
+* - Date types are forgiving, but you should specify ISO8601 and C# will       *
+*   outputs something like "2019-08-01T00:00:00-07:00"                         *
+*                                                                              *
+* - A type with a ? (e.g. int?) means that the JSON property may be missing    *
+*   or null. This usually means that it's an optional property, but it also    *
+*   could mean that it's required only in some contexts                        *
+*                                                                              *
+* - The "required" keyword means that the JSON property should always be       *
+*   present (when listing) or must be specified when creating/editing          *
+*                                                                              *
+*******************************************************************************/
 
+namespace Inedo.ProGet;
+
+// JSON Object used by the Create/Update Project (SCA) HTTP endpoint
 public sealed class ProjectInfo
 {
+    // The internal ID of project
     public int? Id { get; set; }
+
+    // The name of the project.
+    // * Must be unique across the system
     public string? Name { get; set; }
+
+    // SBOM Metadata field
     public string? Type { get; set; }
+
+    // SBOM Metadata field
     public string? Url { get; set; }
 }
