0733743a980e4c3e4b7bb60a0621d65a6e4d8474
Alex Vasse <alexandervasse@gmail.com>
2024-08-01 12:36:43 +0900

    realistic debian feed name

--- pgutil/Packages/DownloadCommand.cs
+++ pgutil/Packages/DownloadCommand.cs
@@ -15,7 +15,7 @@ internal partial class Program
                 
                  > pgutil packages download --feed=approved-npm --package=@babel/runtime --version=7.25.0 --output-file=C:\npm-packages\package.tgz 
                 
-                 > pgutil packages download --feed=myDebianFeed --package= debhelper --version=13.15.3 --component=main --distro=stable --arch=all --output-file=C:\debian-packages\debhelper_13.15.3_all.deb 
+                 > pgutil packages download --feed=public-debian --package= debhelper --version=13.15.3 --component=main --distro=stable --arch=all --output-file=C:\debian-packages\debhelper_13.15.3_all.deb 
 
                 For more information, see: https://docs.inedo.com/docs/proget/reference-api/proget-api-packages/proget-api-packages-download 
                 """;
