f5588617bff2559f2f63fcf9c4249e28ae2cbfed
Alex Vasse <alexandervasse@gmail.com>
2025-12-03 17:49:24 +0900

    added descriptions to .cs files

--- Inedo.ProGet/SecurityTask.cs
+++ Inedo.ProGet/SecurityTask.cs
@@ -1,9 +1,46 @@
-﻿namespace Inedo.ProGet;
+﻿/*******************************************************************************
+* ABOUT THIS FILE                                                              *
+********************************************************************************
+*                                                                              *
+* This file contains C# code to serialize (write) and deserialize (read) JSON  *
+* objects for ProGet HTTP Endpoints. It also serves as the specifications for  *
+* the expected format of these JSON objects; this is why it doesn't follow     *
+* normal C# commenting contentions/standards.                                  *
+*                                                                              *
+* If you're not familiar with JSON Serialization in C#, a few notes:           *
+*                                                                              *
+* - The C# property names are PascalCase, but they are converted to camelCase  *
+*   for JSON; e.g. "MyProperty" will become "myProperty"                       *
+*                                                                              *
+* - Some C# properties will reference enums; these are converted to camelCase  *
+*   string values; e.g. "MyValue" will become "myValue"                        *
+*                                                                              *
+* - Date types are forgiving, but you should specify ISO8601 and C# will       *
+*   outputs something like "2019-08-01T00:00:00-07:00"                         *
+*                                                                              *
+* - A type with a ? (e.g. int?) means that the JSON property may be missing    *
+*   or null. This usually means that it's an optional property, but it also    *
+*   could mean that it's required only in some contexts                        *
+*                                                                              *
+* - The "required" keyword means that the JSON property should always be       *
+*   present (when listing) or must be specified when creating/editing          *
+*                                                                              *
+*******************************************************************************/
 
+namespace Inedo.ProGet;
+
+// JSON Object used by the Create Task HTTP endpoint
 public sealed class SecurityTask
 {
+    // Name given to the task (e.g. "View & Download Packages")
     public required string Name { get; init; }
-    public string? Description { get; init; }
-    public bool FeedScoped { get; init; }
+
+    // Description of the task (e.g. "Allows basic feed access")
+    public string? Description { get; init; } 
+
+    // Indicates if the task is feed-scoped ("true") or global ("false")
+    public bool FeedScoped { get; init; } 
+
+    // The task attributes that the task enables (e.g. ["Feeds_ViewFeed", "Feeds_DownloadPackage"])
     public required string[] Attributes { get; init; }
 }
