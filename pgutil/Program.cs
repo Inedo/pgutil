ae19906112496317dddc364c3cd0d64bafad1c87
Alana Tripp <atripp@inedo.com>
2026-05-26 17:15:28 +0900

    Added version command

--- pgutil/Program.cs
+++ pgutil/Program.cs
@@ -47,6 +47,7 @@ internal sealed partial class Program : IConsoleCommandContainer
     public static void Configure(ICommandBuilder builder)
     {
         builder.WithCommand<SourcesCommand>()
+            .WithCommand<VersionCommand>()
             .WithCommand<ApiKeysCommand>()
             .WithCommand<HealthCommand>()
             .WithCommand<PackagesCommand>()
---
+++ pgutil/Version/VersionCommand.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleMan;
+
+namespace PgUtil;
+
+internal sealed partial class Program
+{
+    internal sealed class VersionCommand : IConsoleCommand
+    {
+        static string IConsoleArgument.Name => "version";
+        static string IConsoleArgument.Description => "Displays the current version";
+
+        static void IConsoleCommand.Configure(ICommandBuilder builder)
+        {
+        }
+        static Task<int> IConsoleCommand.ExecuteAsync(CommandContext context, CancellationToken cancellationToken)
+        {
+            var currentVersion = typeof(Program).Assembly.GetName().Version!;
+            Console.WriteLine(currentVersion.ToString(3));
+            return Task.FromResult(0);
+        }
+    }
+}
